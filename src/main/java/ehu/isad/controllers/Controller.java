e442179c7429b91d7244e8f8a4b6708b3ecaaa7f
Juanan Pereira <juanan@ikasten.io>
2020-08-16 21:45:46 +0200

    Add StackPane

--- src/main/java/ehu/isad/controllers/Controller.java
+++ src/main/java/ehu/isad/controllers/Controller.java
@@ -9,10 +9,7 @@ import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 
 
@@ -37,6 +34,21 @@ public class Controller implements Initializable {
     private Button btnSettings;
 
     @FXML
+    private GridPane pnStudents;
+
+    @FXML
+    private GridPane pnTeachers;
+
+    @FXML
+    private Label pnFees;
+
+    @FXML
+    private GridPane pnUsers;
+
+    @FXML
+    private GridPane pnSettings;
+
+    @FXML
     private Pane pnlStatus;
 
     @FXML
@@ -59,22 +71,27 @@ public class Controller implements Initializable {
             lblStatusMin.setText("/home/students");
             lblStatus.setText("Students");
             pnlStatus.setBackground(new Background(new BackgroundFill(Color.rgb(113, 86, 221), CornerRadii.EMPTY, Insets.EMPTY)));
+            pnStudents.toFront();
         } else if (event.getSource() == btnTeachers) {
             lblStatusMin.setText("/home/teachers");
             lblStatus.setText("Teachers");
             pnlStatus.setBackground(new Background(new BackgroundFill(Color.rgb(43, 63, 99), CornerRadii.EMPTY, Insets.EMPTY)));
+            pnTeachers.toFront();
         } else if (event.getSource() == btnFees) {
             lblStatusMin.setText("/home/fees");
             lblStatus.setText("Fees");
             pnlStatus.setBackground(new Background(new BackgroundFill(Color.rgb(43, 99, 63), CornerRadii.EMPTY, Insets.EMPTY)));
+            pnFees.toFront();
         } else if (event.getSource() == btnUsers) {
             lblStatusMin.setText("/home/users");
             lblStatus.setText("Users");
             pnlStatus.setBackground(new Background(new BackgroundFill(Color.rgb(99, 43, 63), CornerRadii.EMPTY, Insets.EMPTY)));
+            pnUsers.toFront();
         } else if (event.getSource() == btnSettings) {
             lblStatusMin.setText("/home/settings");
             lblStatus.setText("Settings");
             pnlStatus.setBackground(new Background(new BackgroundFill(Color.rgb(42, 28, 66), CornerRadii.EMPTY, Insets.EMPTY)));
+            pnSettings.toFront();
         }
     }
 
